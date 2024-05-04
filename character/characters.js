9306e0dce98c1000ce75e970ed0be0630006a395
isabella cabrera <icsogamoso003@gmail.com>
2024-05-04 14:52:31 -0500

    se hizo la nueva estructura, constrcutor y todo

--- character/characters.js
+++ character/characters.js
@@ -1,50 +1,15 @@
-import { obtenerPersonajes } from "./utils.js";
+import { obtenerPersonajes, Personaje } from "./utils.js";
 
 const renderizarPersonajes = async () => {
-    const crearTarjetaPersonaje = (personaje) => {
-        const productosLista = document.querySelector('.personajes-lista');
-
-        const li = document.createElement('li');
-        li.classList.add('productos__producto');
-
-        const link = document.createElement('a');
-        link.href = `characterdetails.html?id=${personaje.id}`;
-
-        const img = document.createElement('img');
-        img.src = personaje.image;
-        img.alt = personaje.name;
-
-        const h2 = document.createElement('h2');
-        h2.textContent = personaje.name;
-
-        const label = document.createElement('label');
-        label.classList.add('star');
-        label.innerHTML = '<i class="far fa-star"></i>';
-
-        // Manejar eventos de favoritos
-        label.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            label.classList.toggle('checked');
-            // Aquí puedes agregar lógica para manejar la selección como favorito
-            // Por ejemplo, podrías almacenar el ID del personaje en localStorage
-        });
-
-        link.appendChild(img);
-        link.appendChild(h2);
-        link.appendChild(label);
-
-        li.appendChild(link);
-
-        productosLista.appendChild(li);
-    };
+    const contenedorProductos = document.querySelector('.personajes-lista');
 
     const personajes = await obtenerPersonajes();
     for (const personaje of personajes) {
-        crearTarjetaPersonaje(personaje);
+        const nuevoPersonaje = new Personaje(personaje.id, personaje.name, personaje.image);
+        const elementoHTML = nuevoPersonaje.render();
+        contenedorProductos.appendChild(elementoHTML);
     }
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    renderizarPersonajes();
-});
+document.addEventListener("DOMContentLoaded", renderizarPersonajes);
 
--- character/utils.js
+++ character/utils.js
@@ -3,3 +3,43 @@ export const obtenerPersonajes = async () => {
     const data = await response.json();
     return data;
 };
+
+export class Personaje {
+    constructor(id,name,imagen) {
+        this.id = id;
+        this.name = name;
+        this.imagen = imagen;
+    };
+
+    render() {
+        const producto = document.createElement("li");
+        producto.classList.add("productos__producto");
+    
+        const link = document.createElement("a");
+        link.href= `characterdetails.html?id=${this.id}`;
+     
+        const img = document.createElement("img");
+        img.src = this.imagen;
+        this.alt = this.name;
+    
+        const h2 = document.createElement("h2");
+        h2.textContent = this.name;
+    
+        const label = document.createElement("label");
+            label.classList.add("star");
+            label.innerHTML = '<i class="far fa-star"></i>';
+    
+            label.addEventListener('click', function(event) {
+                event.preventDefault(); 
+                label.classList.toggle('checked');
+            });
+    
+            link.appendChild(img);
+            link.appendChild(h2);
+            link.appendChild(label);
+    
+            producto.appendChild(link);
+            return producto;
+    };
+}
+
