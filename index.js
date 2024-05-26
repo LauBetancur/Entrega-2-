0e3746e98c63f8dc53e51bd8eb7caef4baee3b5c
LauBetancur <lau1betancur1@gmail.com>
2024-05-26 14:28:41 -0500

    login/session/register try 1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { obtenerHechizos, Spell } from "./utils-main.js";
+import { obtenerUsuarioEnSesion, logout } from "./session.js";
 
 const renderizarHechizos = async () => {
     const contenedorProductos = document.querySelector('.productos');
-    const contenedorBotones = document.querySelector('.contenedor-botones'); 
+    const contenedorBotones = document.querySelector('.contenedor-botones');
     const btnPrev = document.querySelector('.prev-btn');
     const btnNext = document.querySelector('.next-btn');
 
@@ -17,7 +18,6 @@ const renderizarHechizos = async () => {
     const cards = document.querySelectorAll('.productos__producto');
     const totalCards = cards.length;
 
-    
     const showCards = () => {
         cards.forEach((card, index) => {
             if (index >= currentIndex && index < currentIndex + 3) {
@@ -34,7 +34,7 @@ const renderizarHechizos = async () => {
             showCards();
         }
     };
-   
+
     const prevSlide = () => {
         if (currentIndex > 0) {
             currentIndex--;
@@ -50,4 +50,24 @@ const renderizarHechizos = async () => {
 
     showCards();
 };
-renderizarHechizos();
+
+const gestionarAutenticacion = () => {
+    const authButton = document.getElementById("auth-button");
+
+    const usuarioEnSesion = obtenerUsuarioEnSesion();
+
+    if (usuarioEnSesion) {
+        authButton.innerHTML = '<button type="button" class="log-out">Log Out</button>';
+        authButton.querySelector("button").addEventListener("click", () => {
+            logout();
+            window.location.href = "index.html";
+        });
+    } else {
+        authButton.innerHTML = '<a href="log-in/Log-in.html"><button type="button" class="log-in">Log In</button></a>';
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    gestionarAutenticacion();
+    renderizarHechizos();
+});
